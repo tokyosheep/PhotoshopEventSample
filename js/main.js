216c235e94b763eedf182ca46f2443559dc79f07
tokyosheep <tokyosheep@hotmail.com>
2020-06-30 17:59:21 +0900

    update

--- js/main.js
+++ js/main.js
@@ -9,21 +9,24 @@ window.onload = () =>{
     const flag = document.getElementById("flag");
     const eventButton = document.getElementById("eventButton");
 
+    csInterface.evalScript(`$.evalFile("${toJSX+cropJsx}")`,(e)=>{
+        const event = new CSEvent();
+        event.data = e;
+        event.type = `com.adobe.PhotoshopRegisterEvent`;
+        event.scope = `APPLICATION`; 
+        event.appId = csInterface.getApplicationID();
+        event.extensionId = csInterface.getExtensionID();
+        csInterface.dispatchEvent(event);
+        console.log(csInterface.getApplicationID());    
+        console.log(csInterface.getExtensionID());
+        console.log(event);
+    });
+
     const PhotoshopCallbackUnique = () =>{
         csInterface.evalScript(`$.evalFile("${toJSX+caution}")`);
     }
     const dispatchEvent = () =>{
         flag.textContent = "ON";
-        csInterface.evalScript(`$.evalFile("${toJSX+cropJsx}")`,(e)=>{
-            const event = new CSEvent();
-            event.data = e;
-            event.type = `com.adobe.PhotoshopRegisterEvent`;
-            event.scope = `APPLICATION`; 
-            event.appId = csInterface.getApplicationID();
-            event.extensionId = csInterface.getExtensionID();
-            csInterface.dispatchEvent(event);
-            console.log(event);
-        });
         csInterface.addEventListener(`com.adobe.PhotoshopJSONCallback${extensionId}`,PhotoshopCallbackUnique);
         csInterface.evalScript(`alert("dispatch crop event")`);
     }
